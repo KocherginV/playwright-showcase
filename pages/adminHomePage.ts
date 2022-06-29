a99c4d4174a740414d48133d1c488ae89fddb224
Volodymyr Kochergin <volodymyrkochergin@gmail.com>
2022-06-29 17:55:59 +0200

    add messages tests

--- pages/adminHomePage.ts
+++ pages/adminHomePage.ts
@@ -3,9 +3,31 @@ import { Page, Locator } from '@playwright/test';
 export class AdminHomePage {
     readonly page: Page;
     readonly logoutButton: Locator;
+    readonly message: Locator;
+    readonly messagesLink: Locator;
+    readonly guestName: Locator;
+    readonly messageSubject: Locator;
+    readonly messageModal: Locator;
+    readonly detailedMessageAuthor: Locator;
+    readonly detailedMessageEmail: Locator;
+    readonly detailedMessageSubject: Locator;
+    readonly detailedMessageText: Locator;
+    readonly detailedMessagePhone: Locator;
+    readonly closeMessageModalButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.logoutButton = page.locator('a', { hasText: 'Logout' });
+        this.message = page.locator('#message0');
+        this.messagesLink = page.locator('a[href="#/admin/messages"]');
+        this.guestName = page.locator('div[data-testid="message0"] > p');
+        this.messageSubject = page.locator('div[data-testid="messageDescription0"] > p');
+        this.messageModal = page.locator('div[data-testid="message"]');
+        this.detailedMessageAuthor = page.locator('div[data-testid="message"] > div > .col-10 > p');
+        this.detailedMessageEmail = page.locator('div[data-testid="message"] > div > .col-12 > p').nth(0);
+        this.detailedMessageSubject = page.locator('div[data-testid="message"] > div > .col-12 > p').nth(1);
+        this.detailedMessageText = page.locator('div[data-testid="message"] > div > .col-12 > p').nth(2);
+        this.detailedMessagePhone = page.locator('div[data-testid="message"] > div > .col-2 > p');
+        this.closeMessageModalButton = page.locator('button[class="btn btn-outline-primary"]');
     }
 }
--- tests/adminPage.spec.ts
+++ tests/adminPage.spec.ts
@@ -30,9 +30,42 @@ test.describe('Login tests', () => {
         const adminLoginPage = new AdminLoginPage(page);
         const helper = new Helper(page);
         const adminHomePage = new AdminHomePage(page);
-        
+
         await helper.login(testData.credentials.valid.username, testData.credentials.valid.password);
         await adminHomePage.logoutButton.click();
         await expect(adminLoginPage.loginButton).toBeVisible();
+    });
+});
+
+test.describe('Messages tests', () => {
+
+    test('Can see message', async ({page}) => {
+        const helper = new Helper(page);
+        const adminHomePage = new AdminHomePage(page);
+
+        await helper.login(testData.credentials.valid.username, testData.credentials.valid.password);
+        await adminHomePage.messagesLink.click();
+        await expect(adminHomePage.message).toBeVisible();
+        await expect(adminHomePage.guestName).toHaveText('James Dean');
+        await expect(adminHomePage.messageSubject).toHaveText('Booking enquiry');
+    });
+
+    test('Can read full message',async ({page}) => {
+        const helper = new Helper(page);
+        const adminHomePage = new AdminHomePage(page);
+
+        await helper.login(testData.credentials.valid.username, testData.credentials.valid.password);
+        await adminHomePage.messagesLink.click();
+        await adminHomePage.message.click();
+
+        await expect(adminHomePage.messageModal).toBeVisible();
+        await expect(adminHomePage.detailedMessageAuthor).toHaveText('From: James Dean');
+        await expect(adminHomePage.detailedMessageEmail).toHaveText('Email: james@email.com');
+        await expect(adminHomePage.detailedMessageSubject).toHaveText('Booking enquiry');
+        await expect(adminHomePage.detailedMessageText).toHaveText('I would like to book a room at your place');
+        await expect(adminHomePage.detailedMessagePhone).toHaveText('Phone: 01402 619211');
+
+        await adminHomePage.closeMessageModalButton.click();
+        await expect(adminHomePage.messageModal).not.toBeVisible();
     });
 });
